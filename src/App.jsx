d7e712589fb49928606ee763e9a1800ae654cd32
fatmagad0 <162747851+fatmagad0@users.noreply.github.com>
2025-08-05 02:10:03 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import TodoList1 from "./components/TodoList1"
 import TodoList from "./pages/TodoList"
 function App() {
   return (
--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://todolistback.up.railway.app/api/tasks";
 
 export function getTasks() {
-  return axios.get(`${API_URL}/tasks`);
+  return axios.get(`${API_URL}`);
 }
 
 export function createTask(task) {
-  return axios.post(`${API_URL}/tasks`, task);
+  return axios.post(`${API_URL}`, task);
 }
 
 export function updateTask(id, task) {
-  return axios.put(`${API_URL}/tasks/${id}`, task);
+  return axios.put(`${API_URL}/${id}`, task);
 }
 
 export function deleteTask(id) {
-  return axios.delete(`${API_URL}/tasks/${id}`);
+  return axios.delete(`${API_URL}/${id}`);
 }
